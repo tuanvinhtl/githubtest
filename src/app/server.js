6b0bb99d405a71d9242d699219010d523dd632b4
TuanVinh <tuanvinhtl@gmail.com>
2018-09-16 18:35:29 +0700

    delete file

--- src/app/server.js
+++
@@ -1,9 +0,0 @@
-const express =require('express');
-const app = express();
-
-app.use(expect.static(__dirname + 'dist'));
-app.all('*',(req,res)=>{
-    res.status(200).sendFile(__dirname + '/dist/index.html')
-});
-
-app.listen(process.env.PORT || 8080);
